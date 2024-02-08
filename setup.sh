ad5e8b7b68a0581c66bedb7ee1ac5fd90599da81
schneiderman4907 <155277441+schneiderman4907@users.noreply.github.com>
2024-02-08 08:09:21 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#installs packages
-sudo apt-get install i3 rofi picom nitrogen && tar xpvf configs.tar.xz cd configs && rm /$HOME/.config/i3/config && mv config /$HOME/.config/i3/ && rm /$HOME/.bashrc && mv .bashrc /$HOME/
-
